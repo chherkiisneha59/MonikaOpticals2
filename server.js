93eae8d3c7263297f199c0876e6f95b87570b01b
chherkiisneha59 <chherkiisneha@gmail.com>
2026-04-19 23:12:53 +0530

    Enable JSON pretty-printing

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(cors({
 app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.set('json spaces', 2);
 
 app.use((req, res, next) => {
   console.log(`  → ${req.method} ${req.path}`);
